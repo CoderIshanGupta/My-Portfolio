c9a8ab2a78ff209a39c34c43652d3f8762f7d13f
CoderIshanGupta <guptaishan2609@gmail.com>
2025-12-06 23:22:51 +0530

    Update route.ts

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -37,7 +37,7 @@ export async function POST(req: Request) {
 
     const transporter = nodemailer.createTransport({
       host: process.env.SMTP_HOST,
-      port: Number(process.env.SMTP_PORT) || 465,
+      port: Number(process.env.SMTP_PORT) || 587,
       secure: true,
       auth: {
         user: process.env.SMTP_USER,
